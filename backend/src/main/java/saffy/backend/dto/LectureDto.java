8f2cf9d691cd64be5be92a3d05e43de125ff0447
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-10-21 21:09:55 +0900

    feat: dto 구현

---
+++ backend/src/main/java/saffy/backend/dto/LectureDto.java
@@ -0,0 +1,17 @@
+package saffy.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LectureDto {
+    private Long id;
+    private String name;
+}
