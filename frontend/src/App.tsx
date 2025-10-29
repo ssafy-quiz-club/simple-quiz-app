86927e7c4802ec1f8b735d9c6ffdeaa4a9b87b3a
태선 <taesun4767@gmail.com>
2025-10-29 09:40:46 +0900

    빌드 오류 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,11 @@ import QuizPage from './pages/QuizPage';
 import { GlobalStyle } from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
-import Header from './components/Header';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Header />
       <Routes>
         <Route path="/" element={<QuizPage />} />
         <Route path="/admin/*" element={<AdminPage />} />
